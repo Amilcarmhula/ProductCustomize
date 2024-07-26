7e3dfb1bca7e7ad77cb01837318bfec779cf9643
Amilcar Mula <amilcarmhula.am@gmail.com>
2024-07-26 16:53:30 +0200

    Just an update

--- public/js/test.js
+++ public/js/test.js
@@ -1,8 +1,7 @@
-// const register_form = document.getElementById('register_form')
-//         register_form.addEventListener('submit', event => {
-//             event.preventDefault()
-//             const formData = new FormData(register_form)
-//             const data = Object.fromEntries(formData)
-//             console.log(data)
-//         })
-import 'bootstrap'
+const register_form = document.getElementById('register_form')
+        register_form.addEventListener('submit', event => {
+            event.preventDefault()
+            const formData = new FormData(register_form)
+            const data = Object.fromEntries(formData)
+            console.log(data)
+        })
